fb2075b04162c77fa0d647d6386455fc68ca3309
Chris Micle <CodeMedic42@gmail.com>
2017-02-20 19:52:49 -0500

    More tests and setting to version 1.0.0

--- src/singleSpace.js
+++ src/singleSpace.js
@@ -1,28 +1,37 @@
 const _ = require('lodash');
 
 function SingleSpace(namespace, cb) {
-    // create a unique, global symbol name
-    // -----------------------------------
+    // Must be a non empty string
+    if (!_.isString(namespace) || namespace.length <= 0) {
+        throw new Error('Namespace parameter must be a string with 1 or more characters.');
+    }
 
+    // create a unique, global symbol name
     const moduleInstanceKey = Symbol.for(namespace);
 
     // check if the global object has this symbol
     // add it if it does not have the symbol, yet
-    // ------------------------------------------
-
     const globalSymbols = Object.getOwnPropertySymbols(global);
 
     if (_.isFunction(cb)) {
         const hasInstance = (globalSymbols.indexOf(moduleInstanceKey) > -1);
 
         if (!hasInstance) {
+            const value = cb();
+
+            if (_.isNil(value)) {
+                // If null or undefined is returned then do not define the namespace.
+                return;
+            }
+
             global[moduleInstanceKey] = cb();
         }
+    } else if (!_.isNil(cb)) {
+        // If provided the callback must be a function
+        throw new Error('Callback must be a function');
     }
 
     // define the singleton API
-    // ------------------------
-
     const singleton = {};
 
     Object.defineProperty(singleton, 'instance', {
@@ -32,15 +41,9 @@ function SingleSpace(namespace, cb) {
     });
 
     // ensure the API is never changed
-    // -------------------------------
-
     Object.freeze(singleton);
 
-    // export the singleton API only
-    // -----------------------------
-
-    // export default singleton.instance;
-
+    // export the singleton instance only
     return singleton.instance;
 }
 
--- test/unit/singleSpace.test.js
+++ test/unit/singleSpace.test.js
@@ -34,7 +34,6 @@ describe('Single-Space', function mainTest() {
         expect(ret2).to.equal(42);
     });
 
-
     it('Will not redefine the value', () => {
         const ret = SingleSpace('test4.foo.bar', () => {
             return 42;
@@ -52,5 +51,149 @@ describe('Single-Space', function mainTest() {
 
         expect(ret2).to.equal(42);
         expect(called).to.be.false();
+    });
+
+    it('Call with number', () => {
+        let threw = false;
+
+        try {
+            const ret = SingleSpace(42, () => {
+                expect.fail('Should not call');
+            });
+        } catch(err) {
+            expect(err.message).to.equal('Namespace parameter must be a string with 1 or more characters.');
+            threw = true;
+        }
+
+        expect(threw).to.be.true();
+
+        let called = false;
+    });
+
+    it('Call with boolean', () => {
+        let threw = false;
+
+        try {
+            const ret = SingleSpace(true, () => {
+                expect.fail('Should not call');
+            });
+        } catch(err) {
+            expect(err.message).to.equal('Namespace parameter must be a string with 1 or more characters.');
+            threw = true;
+        }
+
+        expect(threw).to.be.true();
+
+        let called = false;
+    });
+
+    it('Call with empty string', () => {
+        let threw = false;
+
+        try {
+            const ret = SingleSpace('', () => {
+                expect.fail('Should not call');
+            });
+        } catch(err) {
+            expect(err.message).to.equal('Namespace parameter must be a string with 1 or more characters.');
+            threw = true;
+        }
+
+        expect(threw).to.be.true();
+
+        let called = false;
+    });
+
+    it('Call with object', () => {
+        let threw = false;
+
+        try {
+            const ret = SingleSpace({}, () => {
+                expect.fail('Should not call');
+            });
+        } catch(err) {
+            expect(err.message).to.equal('Namespace parameter must be a string with 1 or more characters.');
+            threw = true;
+        }
+
+        expect(threw).to.be.true();
+
+        let called = false;
+    });
+
+    it('Call with function', () => {
+        let threw = false;
+
+        try {
+            const ret = SingleSpace(() => {}, () => {
+                expect.fail('Should not call');
+            });
+        } catch(err) {
+            expect(err.message).to.equal('Namespace parameter must be a string with 1 or more characters.');
+            threw = true;
+        }
+
+        expect(threw).to.be.true();
+
+        let called = false;
+    });
+
+    it('When function returns null', () => {
+        const ret1 = SingleSpace('test5.foo.bar', () => {
+            return null;
+        });
+
+        expect(ret1).to.not.exist();
+
+        const ret2 = SingleSpace('test5.foo.bar', () => {
+            return 42;
+        });
+
+        expect(ret2).to.equal(42);
+    });
+
+    it('When function returns undefined', () => {
+        const ret1 = SingleSpace('test6.foo.bar', () => {
+            return;
+        });
+
+        expect(ret1).to.not.exist();
+
+        const ret2 = SingleSpace('test6.foo.bar', () => {
+            return 42;
+        });
+
+        expect(ret2).to.equal(42);
+    });
+
+    it('When function throws', () => {
+        let threw = false;
+        const error = new Error('Test error');
+
+        try {
+            SingleSpace('test7.foo.bar', () => {
+                throw error;
+            });
+        } catch (err) {
+            threw = true;
+
+            expect(err).to.equal(error);
+        }
+
+        expect(threw).to.be.true();
+    });
+
+    it('When not a function', () => {
+        let threw = false;
+
+        try {
+            SingleSpace('test', 42);
+        } catch (err) {
+            threw = true;
+
+            expect(err.message).to.equal('Callback must be a function');
+        }
+
+        expect(threw).to.be.true();
     });
 });
